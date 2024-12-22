72dfd86f5a9882205459b670f7d366345d2ddca7
Alex Dinh <alex.dinh@hotmail.com>
2024-12-22 22:28:35 +1100

    feat: Google Analytics

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -3,8 +3,9 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import { remarkReadingTime } from './src/utils/readTime.ts'
-
 import react from '@astrojs/react';
+import partytown from '@astrojs/partytown'
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,14 +18,24 @@ export default defineConfig({
             wrap: true
         }
     },
-    integrations: [mdx({
-        syntaxHighlight: 'shiki',
-        shikiConfig: {
-            theme: 'material-theme-palenight',
-            wrap: true
-        },
-        drafts: true
-		}), sitemap(), tailwind(), react()],
+    integrations: [
+        mdx({
+            syntaxHighlight: 'shiki',
+            shikiConfig: {
+                theme: 'material-theme-palenight',
+                wrap: true
+            },
+            drafts: true
+		}),
+        sitemap(),
+        tailwind(),
+        react(),
+        partytown({
+            config: {
+              forward: ["dataLayer.push"],
+            },
+        }),
+    ],
     server: {
         host: '0.0.0.0'
     },
